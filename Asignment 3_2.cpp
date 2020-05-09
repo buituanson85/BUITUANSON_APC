018b6eba8c9f677f29a51d60949a2cf51966d36d
buituanson <64853302+buituanson@users.noreply.github.com>
2020-05-10 00:24:06 +0700

    Nop BT Asignment 3

---
+++ Asignment 3_2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	int st,st1,st2,st3,st4;
+	    st=4000;
+	    st1=(float)(st+st*0.8);
+	    st2=(float)(st1+st1*0.8);
+	    st3=(float)(st2+st2*0.8);
+	    st4=(float)(st3+st3*0.8);
+	printf("Tong so tien thu duoc sau 4 nam la:\n %d $",st4);
+}
